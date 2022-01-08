9cea903d9ee61c17d54bccca86553c7ed38b7012
Anowar <1hafizajr@gmail.com>
2022-01-08 20:45:24 +0600

    get formspy

--- myresume/views.py
+++ myresume/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from .models import Resume
+from .forms import ResumeForm
+from django.views import View
+ 
 # Create your views here.
+
+# def index(request):
+#     form = ResumeForm()
+#     return render(request, 'myapp/index.html', {'form':form})
+
+class Index(View):
+    def get(self, request):
+        form = ResumeForm()
+        return render(request, 'myapp/index.html', {'form':form})
